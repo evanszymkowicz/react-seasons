7d5ed9dcaeb2436e22d0acc69496793a2b6eec55
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-02-04 16:00:06 -0500

    More jsx and styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import './SeasonDisplay.css';
 /*if the month is "greater" than 2 and less than 9
 	will tell the program if it is winter or summer where we are*/
 /*Then use a JS ternary expression of 'is lat greater than 0? Yes: Northern'*/
+const seasonConfig = {
+	summer: {
+		text: "Time to get to the beach",
+		icon: 'sun'
+	},
+	winter: {
+		text: "Bundle up, it is cold out",
+		iconName: 'snowflake'
+	}
+};
+
 const getSeason = (lat, month) => {
 	if (month > 2 && month < 9) {
-		lat > 0 ? 'summer' : 'winter';
+		return lat > 0 ? 'summer' : 'winter';
 	} else {
 		return lat > 0 ? 'winter' : 'summer';
 	}
 };
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
 	const season = getSeason(props.lat, new Date().getMonth());
+	const {text, iconName } = seasonConfig[season]; // {text, icon}
+
 	return (
-		<div>
-		{season === 'winter' ? 'Bundle up, it is cold out!' : 'Grab the shorts!'}
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left huge ${iconName} icon`} />
+			<h1>{text}</h1>
+			<i className={`icon-right huge ${iconName} icon`} />
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 //define the constructor method using props as a standard
 //extends functionality from the .Component base class
 //only time using direct assignment to this.setState
 class App extends React.Component {
 	state = { lat: null, errorMessage: '' };
-
-	//must always call .setState
-	//updated object {} contains the new state
+//must always call .setState
+//updated object {} contains the new state
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
 			position => this.setState({ lat:position.coords.lattitude }),
@@ -19,20 +19,27 @@ class App extends React.Component {
 	}
 
 	componentDidUpdate() {
-		console.log('My component was updated. It has rerendered');
+		console.log('The component was updated. It has rerendered');
 	}
 
-	//conditional rendering
+	renderContent() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <SeasonDisplay lat={this.state.lat} />
+		}
+
+		return <Spinner message="Please enable location services in your browser" />;
+	}
+
 	render() {
-			if (this.state.errorMessage && !this.state.lat) {
-				return <div>Error: {this.state.errorMessage}</div>;
-			}
-
-			if (!this.state.errorMessage && this.state.lat) {
-				return <SeasonDisplay lat={this.state.lat} />
-			}
-
-			return <div>Loading!</div>;
+		return (
+			<div className="border red">
+				{this.renderContent()}
+			</div>
+		);
 	}
 }
 
